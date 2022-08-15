fafab928bcc78bfa7d3a1ae18f459eaced253089
goreleaserbot <goreleaser@carlosbecker.com>
2022-08-15 15:50:23 +0000

    Brew formula update for zitadel version v2.1.1

--- Formula/zitadel.rb
+++ Formula/zitadel.rb
@@ -5,21 +5,21 @@
 class Zitadel < Formula
   desc "Open source identity solution built for the container and cloud era"
   homepage "https://zitadel.com"
-  version "2.1.1-fforootd-patch-1.1"
+  version "2.1.1"
   license "Apache 2.0"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/zitadel/zitadel/releases/download/v2.1.1-fforootd-patch-1.1/zitadel_Darwin_x86_64.tar.gz"
-      sha256 "09a17383b9ac810d7400524ad40992f4f7cb8e2931eaf326bbf8620b312cb008"
+    if Hardware::CPU.arm?
+      url "https://github.com/zitadel/zitadel/releases/download/v2.1.1/zitadel_Darwin_arm64.tar.gz"
+      sha256 "be67854b13ae89c162e5bc3d631c7f5fa6e02fa57da164b7a4e15736b41b7d18"
 
       def install
         bin.install "zitadel"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/zitadel/zitadel/releases/download/v2.1.1-fforootd-patch-1.1/zitadel_Darwin_arm64.tar.gz"
-      sha256 "5727fb2871cb980e5b2f74fcc5bccf4027621281f8920d8cb40a4452cc07682c"
+    if Hardware::CPU.intel?
+      url "https://github.com/zitadel/zitadel/releases/download/v2.1.1/zitadel_Darwin_x86_64.tar.gz"
+      sha256 "5f7aa65e0dcd1df3224066f91f466a7c06408fd341c5fb6c2d4070963acbbba3"
 
       def install
         bin.install "zitadel"
@@ -29,16 +29,16 @@ class Zitadel < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/zitadel/zitadel/releases/download/v2.1.1-fforootd-patch-1.1/zitadel_Linux_x86_64.tar.gz"
-      sha256 "5fa6ff3740053bf093789a64d94bf0ce429e925dd0e437e43b0b4e7b66dcbcef"
+      url "https://github.com/zitadel/zitadel/releases/download/v2.1.1/zitadel_Linux_x86_64.tar.gz"
+      sha256 "5fe7f1f4a5f8ac953bd1ce352ef4d30bc9a4c08180376a12cd6c0501d7b0a91f"
 
       def install
         bin.install "zitadel"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/zitadel/zitadel/releases/download/v2.1.1-fforootd-patch-1.1/zitadel_Linux_arm64.tar.gz"
-      sha256 "d56e1e33ec22436a77e325ac432fdbcea8bb6f0fd210fa1f0ebf50cbed5b4246"
+      url "https://github.com/zitadel/zitadel/releases/download/v2.1.1/zitadel_Linux_arm64.tar.gz"
+      sha256 "d5516955879b6c1ff083b3423022270fc74e0796d1c25cbf70dd0c8860885223"
 
       def install
         bin.install "zitadel"
